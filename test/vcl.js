78d4a0fd403cd69b266b00c27539080620af6f75
Aaron Coburn <acoburn@amherst.edu>
2013-04-18 17:41:06 -0400

    added tests

---
+++ test/vcl.js
@@ -0,0 +1,32 @@
+var common = exports
+  , path   = require('path')
+  , config = require('../config.json');
+
+common.lib = path.join(__dirname, '../lib');
+
+var vcl = require('../');
+
+var monitor = vcl.createMonitor(config);
+
+monitor.on('error', function(err) {
+  console.log("Error :: " + err);
+});
+
+monitor.on('incorrectImage', function(host) {
+  console.log("Incorrect Revision :: " + host);
+});
+
+monitor.on('info', function(msg) {
+  console.log(msg);
+});
+
+monitor.on('failed', function(host) {
+  console.log('Failed :: ' + host);
+});
+
+monitor.on('available', function(host) {
+  console.log('OK :: ' + host);
+});
+
+monitor.check(config.vcl.managementNode);
+      
